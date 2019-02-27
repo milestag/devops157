e80556dcd7343dd6d7bd0057d715d49ad72b5bdb
milestag <primero.tag@gmail.com>
2019-02-27 15:57:15 +0200

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,6 @@
-PowerShell Compress-Archive C:\Users\Miles\source\repos\MyDemo1\MyDemo1\bin\Release\Publish D:\Greg\Publish.zip -Force
-cd D:\Greg
+#This is SCRIPT Example. If you want to use them. Plesse change the all "Paths" according to yours Developement Environment
+PowerShell Compress-Archive C:\%VisualStudioPublishPath%\bin\Release\Publish D:\%VagrantFolder%\Publish.zip -Force
+cd D:\%VagrantFolder%\
 D:
 Vagrant up --provision
 
